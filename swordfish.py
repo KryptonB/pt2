cf00279fdab2cee3a3583ff6fab590ecd9c83e16
Shamal <sratnappuli@virtusa.com>
2018-06-26 06:39:03 +0530

    Changed swordfish.py

--- swordfish.py
+++ swordfish.py
@@ -3,8 +3,12 @@ while True:
     name = input()
     if name != 'Joe':
         continue
-    print('Hello Joe, what is the password? (It is a fish)')
-    password = input()
-    if password == 'swordfish':
-        break
+    while True:
+        print('Hello Joe, what is the password? (It is a fish)')
+        password = input()
+        if password != 'swordfish':
+            continue
+        else:
+            break
+    break
 print('Access granted.')
