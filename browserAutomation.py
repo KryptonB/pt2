1a076932e4dc07c4b73d78b9133391e7c21401c1
Shamal <sratnappuli@virtusa.com>
2018-07-29 21:48:00 +0530

    Chanded python script to clear the username before typing the values

--- browserAutomation.py
+++ browserAutomation.py
@@ -1,18 +1,25 @@
 from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
 
 waitInterval = 30
 
-browser = webdriver.Chrome('C:\\hehe\\chromedriver.exe')
+options = webdriver.ChromeOptions()
+#options.add_argument('user-data-dir=C:\\Users\\sratnappuli\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\')
+options.add_argument('user-data-dir=C:\\mybrowser\\')
+browser = webdriver.Chrome(executable_path='C:\\hehe\\chromedriver.exe', chrome_options=options)
 browser.get('https://fss.virtusa.com/dashboard/')
 
 username = browser.find_element_by_id('userName')
+username.click()
+username.clear()
 username.send_keys('support')
 
 password = browser.find_element_by_id('userPassword')
+password.clear()
 password.send_keys('Kan3lbull3')
 
 loginButton = browser.find_element_by_id('loginbuttonID')
 loginButton.click()
 
 
-browser.wait = WebDriverWait(driver,waitInterval)
+#browser.wait = WebDriverWait(driver,waitInterval)
