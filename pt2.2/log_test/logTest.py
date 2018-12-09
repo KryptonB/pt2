fe9c55c22a9584cc3bba7fc298f4956fcb5d6f4e
nagasurya <shmlrtnppl@gmail.com>
2018-12-09 08:22:07 +0530

    Deleted old log files and added new files. Also modified logTest.py to accept a datestamp as the log file name

--- pt2.2/log_test/logTest.py
+++ pt2.2/log_test/logTest.py
@@ -1,4 +1,6 @@
-import logging, time
+import logging
+import time
+import datetime
 
 # default log level
 logLevel = logging.DEBUG
@@ -6,7 +8,14 @@ logLevel = logging.DEBUG
 # log file path and name
 logFile = 'log/test.log'
 
-# logging formats
+# if you want to add the datetimestamp to the log file, do this
+# and use logFileWithDatestamp as the log file name
+now = datetime.datetime.now()
+fileDatestamp = now.strftime('%Y-%m-%d_%H%M%S')
+logFileWithDatestamp = 'log/test_' + fileDatestamp + '.log'
+
+
+# differnt logging formats. Choose the one you like
 #logFormat = '[ %(asctime)s ] %(levelname)s [%(name)s.%(funcName)s:%(lineno)d] %(message)s'
 #logFormat = '[%(asctime)s] %(levelname)s [Line: %(lineno)d] %(message)s'
 logFormat = '[%(asctime)s] %(levelname)s (%(filename)s: %(lineno)d) %(message)s'
