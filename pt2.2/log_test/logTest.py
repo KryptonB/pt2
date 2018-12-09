0d755c56bf5c29bffc1534d7ecd8ef0cc1e0b4db
nagasurya <shmlrtnppl@gmail.com>
2018-12-09 08:04:48 +0530

    Added few variables to store values for the log formatter

--- pt2.2/log_test/logTest.py
+++ pt2.2/log_test/logTest.py
@@ -1,9 +1,18 @@
 import logging, time
 
+# default log level
+logLevel = logging.DEBUG
+
+# log file path and name
+logFile = 'log/test.log'
+
+# logging formats
 #logFormat = '[ %(asctime)s ] %(levelname)s [%(name)s.%(funcName)s:%(lineno)d] %(message)s'
-logFormat = '[%(asctime)s] %(levelname)s (%(filename)s: %(lineno)d) %(message)s'
 #logFormat = '[%(asctime)s] %(levelname)s [Line: %(lineno)d] %(message)s'
-logging.basicConfig(filename='hehe.log', level=logging.DEBUG, format=logFormat)
+logFormat = '[%(asctime)s] %(levelname)s (%(filename)s: %(lineno)d) %(message)s'
+
+
+logging.basicConfig(filename=logFile, level=logLevel, format=logFormat)
 
 def repea():
     while True :
